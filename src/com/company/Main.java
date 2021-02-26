94563067fe940da6bb1890a8d569fd0cfc965734
Szakol <Szakol@users.noreply.github.com>
2021-02-26 17:33:24 +0100

    commit v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Animals dog = new Animals("dog", 20.5, "Krzyś");
         dog.write();
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        dog.write();
+        Animals horse = new Animals("mustang", 200, "rafał");
+        horse.write();
     }
 }
